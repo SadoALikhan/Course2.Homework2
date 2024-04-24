ad3485f8b7ef9fb92703cc2dd7f53012fd4b737f
SadoALikhan <khan.ali.99.sado@mail.ru>
2024-04-24 15:04:22 +0300

    Homework2

---
+++ src/Hogwarts.java
@@ -0,0 +1,12 @@
+public abstract class Hogwarts {
+    int powerOfMagic;
+    int apparition;
+
+    public Hogwarts(int powerOfMagic, int apparition) {
+        this.powerOfMagic = powerOfMagic;
+        this.apparition = apparition;
+    }
+
+    public void compare(Hogwarts student, Hogwarts student1) {
+    }
+}
