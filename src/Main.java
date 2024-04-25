049c4a3a17124a6b9dff82c80b7bb0fd6856deb6
SadoALikhan <khan.ali.99.sado@mail.ru>
2024-04-25 19:27:05 +0300

    Homework2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Hogwarts[] student = new Hogwarts[]{
-                new Gryffindor ("Гарри Поттер", 88, 79, 81, 56, 61),
-                new Gryffindor("Гермиона Грейнджер", 77, 81, 66, 54, 46),
-                new Gryffindor("Рон Уизли", 66, 69, 55, 46, 71),
+        Hogwarts[] student = new Hogwarts[12];
+                student[0] = new Gryffindor("Гарри Поттер", 88, 79, 81, 56, 61);
+                new Gryffindor("Гермиона Грейнджер", 77, 81, 66, 54, 46);
+                new Gryffindor("Рон Уизли", 66, 69, 55, 46, 71);
                 new Slytherin("Драко Малфой", 81, 82, 44,
-                        77, 64, 71, 51),
+                        77, 64, 71, 51);
                 new Slytherin("Грэхэм Монтегю", 77, 89, 45,
-                        33, 91, 66, 77),
+                        33, 91, 66, 77);
                 new Slytherin("Грегори Гойл", 67, 76, 87,
-                        78, 54, 45, 61),
-                new Hufflepuff("Захария Смит", 45, 54, 63, 74, 81),
-                new Hufflepuff("Седрик Диггори", 65, 56, 76, 67, 88),
-                new Hufflepuff("Джастин Финч-Флетчли", 44, 55, 63, 76, 82),
-                new Ravenclaw("Чжоу Чанг", 56, 74, 33, 39, 41, 80),
-                new Ravenclaw("Падма Патил", 76, 39, 54, 44, 59, 69),
-                new Ravenclaw("Маркус Белби", 71, 41, 31, 51, 61, 81)
-        };
+                        78, 54, 45, 61);
+                new Hufflepuff("Захария Смит", 45, 54, 63, 74, 81);
+                new Hufflepuff("Седрик Диггори", 65, 56, 76, 67, 88);
+                new Hufflepuff("Джастин Финч-Флетчли", 44, 55, 63, 76, 82);
+                new Ravenclaw("Чжоу Чанг", 56, 74, 33, 39, 41, 80);
+                new Ravenclaw("Падма Патил", 76, 39, 54, 44, 59, 69);
+                new Ravenclaw("Маркус Белби", 71, 41, 31, 51, 61, 81);
     }
 }
